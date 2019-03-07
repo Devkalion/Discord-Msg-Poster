798a2bb4918742bc8f92b096e652effa5f8add6f
Rustan <rustanrr@mail.ru>
2019-03-07 20:41:34 +0400

    add simple console interface

--- src/start.py
+++ src/start.py
@@ -1,6 +1,5 @@
 import json
 import os
-import sys
 
 import discord
 
@@ -25,32 +24,40 @@ def make_message(message_config):
     return content, embed
 
 
-@client.event
-async def on_ready():
+async def post(test=True):
     for message_file in get_message_files():
         message_config = get_message_config(message_file)
-
-        message_config['destination'] = client.get_channel(message_config.get('destination', destination))
+        if test:
+            destination = settings.TEST_DESTINATION
+        else:
+            destination = message_config.get('destination', settings.DEFAULT_DESTINATION)
+        message_config['destination'] = client.get_channel(destination)
         if 'embed' in message_config:
             embed_config = message_config['embed']
             embed_config['timestamp'] = discord.Embed.Empty  # Not supported yet
             message_config['embed'] = discord.Embed.from_data(embed_config).set_footer(**embed_config['footer'])
         await client.send_message(**message_config)
-    await client.logout()
     print('Messages posted successfully')
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print('Usage:\npython message test\npython message post')
-    elif len(sys.argv) > 2:
-        print(f'Expected 1 argument but {len(sys.argv)} were given')
-    else:
-        if sys.argv[1] == 'test':
-            destination = settings.TEST_DESTINATION
-        elif sys.argv[1] == 'post':
-            destination = settings.DEFAULT_DESTINATION
+@client.event
+async def on_ready():
+    valid_commands = ('post', 'test', 'exit')
+    print('Bot connected', 'Enter your commands', sep='\n')
+    while True:
+        command = input('> ')
+
+        if command not in valid_commands:
+            print('Invalid command')
+            print('Valid commands are:', ', '.join(valid_commands))
+        elif command == 'exit':
+            break
         else:
-            print('Usage:\npython message test\npython message post')
-            exit(1)
-        client.run(settings.TOKEN)
+            await post(command == 'test')
+    await client.logout()
+
+
+if __name__ == '__main__':
+    print('Waiting while bot connecting')
+    client.run(settings.TOKEN)
+    print('Bot disconnected')
