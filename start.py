ca0c07e6062831ede62a84d5e21f2260fd315ca3
Rustan <rustanrr@mail.ru>
2019-03-07 21:12:55 +0400

    add env

--- start.py
+++ start.py
@@ -3,19 +3,18 @@ import os
 import shutil
 
 import discord
-
-import settings
+import dotenv
 
 client = discord.Client()
 
 
 def get_message_files():
-    files = os.listdir('../data')
+    files = os.listdir('data')
     return filter(lambda x: x.endswith('.json'), files)
 
 
 def get_message_config(path):
-    with open(f'../data/{path}', encoding='utf8') as f:
+    with open(f'data/{path}', encoding='utf8') as f:
         return json.load(f)
 
 
@@ -29,9 +28,9 @@ async def post(test=True):
     for message_file in get_message_files():
         message_config = get_message_config(message_file)
         if test:
-            destination = settings.TEST_DESTINATION
+            destination = os.environ['TEST_DESTINATION']
         else:
-            destination = message_config.get('destination', settings.DEFAULT_DESTINATION)
+            destination = message_config.get('destination', os.environ['DEFAULT_DESTINATION'])
         message_config['destination'] = client.get_channel(destination)
         if 'embed' in message_config:
             embed_config = message_config['embed']
@@ -46,6 +45,7 @@ async def on_ready():
     valid_commands = ('post', 'test', 'exit')
     print('Bot connected', 'Enter your commands', sep='\n')
     while True:
+        dotenv.load_dotenv()
         command = input('> ')
 
         if command not in valid_commands:
@@ -56,12 +56,13 @@ async def on_ready():
             if not len(messages):
                 break
             if input('Save data? (N) ') in ('Y', 'y', 'yes', 'Yes'):
-                dirname = input(f'Enter dirname ({settings.DEFAULT_DIR_NAME}): ') or settings.DEFAULT_DIR_NAME
-                os.makedirs(f'../data/{dirname}', exist_ok=True)
+                dirname = os.environ.get('DEFAULT_DIR_NAME', 'dir')
+                dirname = input(f'Enter dirname ({dirname}): ') or dirname
+                os.makedirs(f'data/{dirname}', exist_ok=True)
                 for message in messages:
-                    shutil.copy2(f'../data/{message}', f'../data/{dirname}/{message}')
+                    shutil.copy2(f'data/{message}', f'data/{dirname}/{message}')
             for message in messages:
-                os.remove(f'../data/{message}')
+                os.remove(f'data/{message}')
             break
         else:
             await post(command == 'test')
@@ -69,6 +70,8 @@ async def on_ready():
 
 
 if __name__ == '__main__':
+    dotenv.load_dotenv()
+    TOKEN = os.environ['TOKEN']
     print('Waiting while bot connecting')
-    client.run(settings.TOKEN)
+    client.run(TOKEN)
     print('Bot disconnected')
