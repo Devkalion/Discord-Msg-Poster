ec9c2036718d37fe3330f6423f4be886b5822b6e
Rustan Rafikov <rustan.rafikov@opendev.com>
2019-03-14 16:14:12 +0400

    add sending files functional

--- start.py
+++ start.py
@@ -9,8 +9,8 @@ client = discord.Client()
 
 
 def get_message_files():
-    files = os.listdir('data')
-    return filter(lambda x: x.endswith('.json'), files)
+    files = sorted(os.listdir('data'))
+    return filter(lambda x: not x.endswith('.gitignore') and not x.endswith('.example'), files)
 
 
 def get_message_config(path):
@@ -24,15 +24,46 @@ def make_message(message_config):
     return content, embed
 
 
+async def post_message(message_file, test):
+    message_config = get_message_config(message_file)
+    if test:
+        message_config['channel_id'] = os.environ['TEST_DESTINATION']
+    else:
+        message_config['channel_id'] = message_config.get('channel_id', os.environ['DEFAULT_DESTINATION'])
+    await client.http.send_message(**message_config)
+
+
+async def send_file(file_name, test):
+    if test:
+        channel_id = os.environ['TEST_DESTINATION']
+    else:
+        channel_id = os.environ['DEFAULT_DESTINATION']
+    with open(f'data/{file_name}', 'rb') as f:
+        content = f.read()
+    await client.http.send_files(channel_id, files=[(content, file_name)])
+
+
 async def post(test=True):
     for message_file in get_message_files():
-        message_config = get_message_config(message_file)
-        if test:
-            message_config['channel_id'] = os.environ['TEST_DESTINATION']
+        if message_file.endswith('.json'):
+            await post_message(message_file, test)
         else:
-            message_config['channel_id'] = message_config.get('channel_id', os.environ['DEFAULT_DESTINATION'])
-        await client.http.send_message(**message_config)
+            await send_file(message_file, test)
     print('Messages posted successfully')
+
+
+def save_data():
+    messages = list(get_message_files())
+    if not len(messages):
+        return
+    if input('Save data? (N) ') in ('Y', 'y', 'yes', 'Yes'):
+        dirname = os.environ.get('DEFAULT_DIR_NAME', 'dir')
+        dirname = input(f'Enter dirname ({dirname}): ') or dirname
+        os.makedirs(f'data/{dirname}', exist_ok=True)
+        for message in messages:
+            shutil.copy2(f'data/{message}', f'data/{dirname}/{message}')
+    for message in messages:
+        os.remove(f'data/{message}')
 
 
 @client.event
@@ -47,17 +78,7 @@ async def on_ready():
             print('Invalid command')
             print('Valid commands are:', ', '.join(valid_commands))
         elif command == 'exit':
-            messages = list(get_message_files())
-            if not len(messages):
-                break
-            if input('Save data? (N) ') in ('Y', 'y', 'yes', 'Yes'):
-                dirname = os.environ.get('DEFAULT_DIR_NAME', 'dir')
-                dirname = input(f'Enter dirname ({dirname}): ') or dirname
-                os.makedirs(f'data/{dirname}', exist_ok=True)
-                for message in messages:
-                    shutil.copy2(f'data/{message}', f'data/{dirname}/{message}')
-            for message in messages:
-                os.remove(f'data/{message}')
+            save_data()
             break
         else:
             await post(command == 'test')
